b10622d9e013d4b64e68202a2bcd0b2540d5a017
07HaleemaZehera <hzehera07@gmail.com>
2022-05-05 11:03:36 +0530

    first commit

---
+++ 11 hrs/REACT_OFFICIAL/src/Container/HomeCont.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import Cart from "../component/Cart"
+
+// export default function HomeCont() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+
+export default  Cart;
---
+++ 11 hrs/REACT_OFFICIAL/src/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+// import "./Main.css"
+
+export default function MainContent() {
+  return (
+    <div>
+     {/* <div className='navbar'>
+     <nav className='nav'>
+          <h1>React </h1>
+          <ul className='lists' >
+              <li>Menu</li>
+              <li>About</li>
+              <li>Contact</li>
+          </ul>
+      </nav> */}
+     {/* </div> */}
+     
+    
+    </div>
+  )
+}
